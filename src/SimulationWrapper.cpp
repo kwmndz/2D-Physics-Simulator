7bd34e6bbb2c7b31e34e21ceaede404d30a3d7d5
kwmndz <kwmndz@umich.edu>
2024-10-26 15:22:09 -0400

    Added optional args for loading/saving a color set from a .txt, or changing the color of the paintbrush or their default color

--- src/SimulationWrapper.cpp
+++ src/SimulationWrapper.cpp
@@ -2,13 +2,40 @@
 #include <vector>
 #include <memory>
 #include <iostream>
+#include <fstream>
 #include <cmath>
+#include <sstream>
 #include "../include/CollisionShapes.hpp"
 #include "../include/ConfigurationWindow.hpp"
 #include "../include/SimulationWindow.hpp"
 
-int main()
+// Saves the indices of the balls that have been colored by left clicking
+void saveToFile(std::string filename, std::vector<int> ballIndicesColored);
+// Loads the indices of the balls
+void loadFromFile(std::string filename, std::vector<int>& ballIndicesColored);
+// Compiles the inputs from the command line
+bool compileInputs(std::string& output_file, std::string& input_file, int& color, int& pcolor, int argc, char** argv);
+// Returns sf color object based on the input color
+sf::Color getColor(int color);
+
+int main(int argc, char** argv)
 {
+    std::string output_file = "";
+    std::string input_file = "";
+    int color = -1;
+    int pcolor = 0;
+
+    if (argc > 10) {
+        std::cerr << "Usage: -output <output_file> -input <input_file> -color <color> -pcolor <color>\n";
+        std::cerr << "Colors: -1 = random, 0 = black, 1 = white, 2 = red, 3 = green, 4 = blue, 5 = yellow, 6 = magenta, 7 = cyan, 8 = orange, 9 = purple\n";
+        // std::cout << argc << std::endl;
+        return -1;
+    }
+
+    if (compileInputs(output_file, input_file, color, pcolor, argc, argv)) {
+        return 0;
+    }
+
     const std::vector<unsigned int> ACCEPTABLE_UNICODE = {8, 32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 46};
     const float TIME_SUB_STEPS = 8.0f;
     const float GRAVITY = 9.81f;
@@ -73,7 +100,11 @@ int main()
     int count = 0;
 
     bool isLeftClicked = false;
-    sf::Color leftClickColor = sf::Color::Black;
+    sf::Color leftClickColor = getColor(pcolor);
+
+    std::vector<int> ballIndicesColored;
+
+    loadFromFile(input_file, ballIndicesColored);
 
     while (simWin1.isOpen() || configWin1.isOpen()) 
     {
@@ -92,6 +123,10 @@ int main()
                 simWin1.isFocused = false;
             }
             if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape) {
+                saveToFile(output_file, ballIndicesColored);
+                if (output_file != "") {
+                    std::cout << "Saved to file: " << output_file << std::endl;
+                }
                 simWin1.close();
                 break;
             }
@@ -111,7 +146,8 @@ int main()
                         auto it = std::find(simWin1.nonStaticObjects.begin(), simWin1.nonStaticObjects.end(), ball);
                         if (it != simWin1.nonStaticObjects.end()) {
                             int index = std::distance(simWin1.nonStaticObjects.begin(), it);
-                            std::cout << "Ball index: " << index << std::endl;
+                            // std::cout << "Ball index: " << index << std::endl;
+                            ballIndicesColored.push_back(index);
                         }
                         ball->setFillColor(leftClickColor);
                         break;
@@ -121,6 +157,9 @@ int main()
             if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Right) {
                 sf::Vector2f mousePos = simWin1.mapPixelToCoords(sf::Mouse::getPosition(simWin1));
                 Ball tinyBall(radius, 30, mousePos, 1, colors[count % numColors], sf::Color::White);
+                if (color != -1) {
+                    tinyBall.setFillColor(getColor(color));
+                }
                 simWin1.add_non_static_object(std::make_shared<Ball>(tinyBall));
                 count++;
             }
@@ -222,8 +261,13 @@ int main()
         if (acc2 >= 0.01 and count < 500) {
             float angle = count * (M_PI / 4); // Rotate by 45 degrees for each ball
             float xPos = 400 + 100 * std::cos(angle); 
-            //float yPos = 300 + 100 * std::sin(angle); 
+            //float yPos = 300 + 100 * std::sin(angle);
             Ball tinyBall(radius, 30, sf::Vector2f(xPos, 56), 1, colors[count % numColors], sf::Color::White);
+            if ( std::find(ballIndicesColored.begin(), ballIndicesColored.end(), count) != ballIndicesColored.end()) {
+                tinyBall.setFillColor(leftClickColor);
+            } else if(color != -1) {
+                tinyBall.setFillColor(getColor(color));
+            }
             //tinyBall.accelerate(sf::Vector2f(100 * std::cos(angle), 100 * std::sin(angle))); // Shoot out in rotating directions
             simWin1.add_non_static_object(std::make_shared<Ball>(tinyBall));
             simWin1.apply_acceleration(sf::Vector2f(100000 * std::cos(angle), 100000 * 2), simWin1.nonStaticObjects.size() - 1);
@@ -263,4 +307,97 @@ int main()
     }
 
     return 0;
+}
+
+void saveToFile(std::string filename, std::vector<int> ballIndicesColored) {
+    std::ofstream outFile(filename);
+    if (outFile.is_open()) {
+        for (const auto& index : ballIndicesColored) {
+            if (index == ballIndicesColored.back()) {
+                outFile << index;
+                break;
+            }
+            outFile << index << ",";
+        }
+        outFile.close();
+    } else {
+        std::cerr << "Unable to open file for saving indecies\n";
+    }
+}
+
+void loadFromFile(std::string filename, std::vector<int>& ballIndicesColored) {
+    std::ifstream inFile(filename);
+    if (inFile.is_open()) {
+        std::string line;
+        std::getline(inFile, line);
+        std::stringstream ss(line);
+        std::string token;
+        while (std::getline(ss, token, ',')) {
+            // std::cout << "Token: " << token << std::endl;
+            ballIndicesColored.push_back(std::stoi(token));
+        }
+        inFile.close();
+    } else {
+        std::cerr << "Unable to open file for loading indecies\n";
+    }
+}
+
+bool compileInputs(std::string& output_file, std::string& input_file, int& color, int& pcolor, int argc, char** argv) {
+    for (int i = 1; i < argc; i++) {
+        if (std::string(argv[i]) == "--help") {
+            std::cout << "Usage: -output <output_file> -input <input_file> -color <color> -pcolor <color>\n";
+            std::cout << "Colors: -1 = random, 0 = black, 1 = white, 2 = red, 3 = green, 4 = blue, 5 = yellow, 6 = magenta, 7 = cyan, 8 = orange, 9 = purple\n";
+            return true;
+        } else if (std::string(argv[i]) == "-output") {
+            if (i + 1 < argc) {
+                output_file = argv[i + 1];
+                std::cout << "Output file: " << output_file << std::endl;
+            }
+        } else if (std::string(argv[i]) == "-input") {
+            if (i + 1 < argc) {
+                input_file = argv[i + 1];
+                std::cout << "Input file: " << input_file << std::endl;
+            }
+        } else if (std::string(argv[i]) == "-color") {
+            if (i + 1 < argc) {
+                color = std::stoi(argv[i + 1]);
+                std::cout << "Color: " << color << std::endl;
+            }
+        } else if (std::string(argv[i]) == "-pcolor") {
+            if (i + 1 < argc) {
+                pcolor = std::stoi(argv[i + 1]);
+                std::cout << "PColor: " << pcolor << std::endl;
+            }
+        }
+    }
+    return false;
+}
+
+sf::Color getColor(int color) {
+    switch (color) {
+        case -1:
+            return sf::Color(rand() % 256, rand() % 256, rand() % 256);
+        case 0:
+            return sf::Color::Black;
+        case 1:
+            return sf::Color::White;
+        case 2:
+            return sf::Color::Red;
+        case 3:
+            return sf::Color::Green;
+        case 4:
+            return sf::Color::Blue;
+        case 5:
+            return sf::Color::Yellow;
+        case 6:
+            return sf::Color::Magenta;
+        case 7:
+            return sf::Color::Cyan;
+        case 8:
+            return sf::Color(255, 165, 0);
+        case 9:
+            return sf::Color(128, 0, 128);
+        default:
+            return sf::Color::Black;
+    }
 }
